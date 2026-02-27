01dda86f23000eb43fd50b57bc2f2769de3195ee
azastel09-eng <az.astel09@gmail.com>
2026-02-27 18:51:47 +0000

    Add files via upload

---
+++ CV.py
@@ -0,0 +1,53 @@
+import streamlit as st
+
+st.title("CV : CURRICULUM VITAE")
+
+with st.sidebar:
+    st.markdown("Aissatou")
+    st.markdown("DIALLO")
+    st.markdown("Email:az.aissa.09@gmail.com")
+    st.markdown("Contact: 778538765")
+
+
+#=============================================================
+
+st.write("PROJET")
+
+
+st.markdown("""Aissatou Diallo Géomaticienne passionée par la géomatique et les systèmes d'information ,je souhaite rejoindre une grande entreprise pour y occuper le posste de géomaticienne ou j'apporterai mon excellent savoir-faire et ma parfaite connaissance des collectes et exploitation des données géographiques.
+
+""")
+#==================================================
+
+st.write("COMPETENCES")
+
+st.write("Connaissances en QGIS")
+st.write("Connaissances en ARCMAP")
+st.markdown("""maitrise des Suites Burautiques ,Connaissance en informatique et en Collecte de données avec l'application Mobile Topographer
+""")
+
+#====================================================
+
+
+st.write("FORMATION")
+
+
+st.write("BTS en cours")
+st.write("Baccalauréat en 2021")
+st.write("BFEM en 2017")
+
+#============================================================
+
+
+st.write("LANGUES")
+
+st.write("Français excellent")
+st.write("Espagnol moyen ") 
+st.write("Bambara avancé ")
+st.write("Peulh avancé")
+#========================================================================================
+
+st.write("DIVERS")
+
+st.markdown("""propose des cours de vacances, j'apprends à approfondir mon oral en ESPAGNOL
+""")
