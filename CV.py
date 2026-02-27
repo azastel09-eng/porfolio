1cd95f16d0b9592af5bc7011af98653e1a3cc74c
azastel09-eng <az.astel09@gmail.com>
2026-02-27 19:09:06 +0000

    Update CV.py

--- CV.py
+++ CV.py
@@ -1,30 +1,33 @@
 import streamlit as st
 
-st.title("CV : CURRICULUM VITAE")
-
 with st.sidebar:
     st.markdown("Aissatou")
     st.markdown("DIALLO")
-    st.markdown("Email:az.aissa.09@gmail.com")
+    st.markdown("az.astel09@gmail.com")
     st.markdown("Contact: 778538765")
 
 
 #=============================================================
 
-st.write("PROJET")
+st.title("A propos de moi")
 
 
-st.markdown("""Aissatou Diallo Géomaticienne passionée par la géomatique et les systèmes d'information ,je souhaite rejoindre une grande entreprise pour y occuper le posste de géomaticienne ou j'apporterai mon excellent savoir-faire et ma parfaite connaissance des collectes et exploitation des données géographiques.
-
+st.markdown("""Géomaticienne passionée par la géographie et les systèmes d'information ,je souhaite rejoindre une grande entreprise pour y occuper le posste de géomaticienne ou j'apporterai mon excellent savoir-faire et ma parfaite connaissance des collectes et exploitation des données géographiques et ma connaissance en conception de plans d'architecturaux.
 """)
 #==================================================
 
 st.write("COMPETENCES")
 
-st.write("Connaissances en QGIS")
-st.write("Connaissances en ARCMAP")
-st.markdown("""maitrise des Suites Burautiques ,Connaissance en informatique et en Collecte de données avec l'application Mobile Topographer
+st.markdown("""Maitrise en cartographie 
+avec QGIS,ARCMAP""")
+st.write("Programmation(python /streamlit )")
+st.write("Topographie")
+st.markdown("""Connaissances en Suites Burautiques ,
+Connaissance en Collecte de données avec l'application Mobile Topographer
 """)
+st.markdown("""maitrise d'Autocad et
+            Sketchup pour la modélisation 3D des maisons""")
+            
 
 #====================================================
 
@@ -32,22 +35,15 @@ st.markdown("""maitrise des Suites Burautiques ,Connaissance en informatique et 
 st.write("FORMATION")
 
 
-st.write("BTS en cours")
-st.write("Baccalauréat en 2021")
-st.write("BFEM en 2017")
-
-#============================================================
+st.write("Brevet de technicien supérieur")
+st.write("Baccalauréat")
 
 
-st.write("LANGUES")
-
-st.write("Français excellent")
-st.write("Espagnol moyen ") 
-st.write("Bambara avancé ")
-st.write("Peulh avancé")
+#===========================================================
 #========================================================================================
 
 st.write("DIVERS")
 
-st.markdown("""propose des cours de vacances, j'apprends à approfondir mon oral en ESPAGNOL
+st.markdown("""Approfondir la recherche sur les collctes et l'analyse de donnéés mais également en
+Conception de plans Architecturaux
 """)
